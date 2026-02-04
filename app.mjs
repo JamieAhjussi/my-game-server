2afc589e36ab5a55fa1cf3a4e01ad04e3cfcfb9e
James <jamespond707@gmail.com>
2026-02-04 17:30:13 +0700

    feat: Add /posts endpoint for creating new posts in the database and remove the dummy /profiles endpoint.

--- app.mjs
+++ app.mjs
@@ -1,5 +1,6 @@
 import e from "express";
 import c from "cors";
+import connectionPool from "./utils/db.mjs";
 
 const app = e();
 
@@ -9,13 +10,35 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.get("/profiles", (req, res) => {
-    return res.json({
-        name: "John Doe",
-        age: 30,
-        email: "[EMAIL_ADDRESS]"
+app.post("/posts", async (req, res) => {
+
+    const newPost = req.body;
+
+    try {
+        const query= `INSERT INTO posts (title, image, category_id, description, content, status_id)
+        VALUES ($1, $2, $3, $4, $5, $6)`;
+
+        const values = [
+            newPost.title,
+            newPost.image,
+            newPost.category_id,
+            newPost.description,
+            newPost.content,
+            newPost.status_id
+        ];
+    await connectionPool.query(query, values);
+    } catch {
+        return res.status(500).json({
+            message : "Database connection error, failed to create new post."
+        })
+    }
+    return res.status(201).json({
+        message : "Post created succesfully"
     });
 });
+
+
+
 
 export default app;
 
